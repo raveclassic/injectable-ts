e45294deb68f58ff72d0e7213019ac1da1f61b62
raveclassic <raveclassic@gmail.com>
2022-12-15 15:12:42 +0300

    style: enable eslint in jest.config.ts

--- packages/fp-ts/jest.config.ts
+++ packages/fp-ts/jest.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default {
   displayName: 'fp-ts',
   preset: '../../jest.preset.js',
--- packages/react/jest.config.ts
+++ packages/react/jest.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default {
   displayName: 'react',
   preset: '../../jest.preset.js',
