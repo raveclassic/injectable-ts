c7e42b1760790f000b2121e74d259aba4c93e8bc
raveclassic <raveclassic@gmail.com>
2022-12-18 10:38:11 +0300

    feat(analyzer): initial vite setup

---
+++ packages/analyzer/src/app/app.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import NxWelcome from './nx-welcome'
+
+const StyledApp = styled.div`
+  // Your style here
+`
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="analyzer" />
+    </StyledApp>
+  )
+}
---
+++ packages/analyzer/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import * as ReactDOM from 'react-dom/client'
+
+import { App } from './app/app'
+
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
