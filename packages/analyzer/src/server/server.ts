d23ef63a1a320dfd7169eb4d8904aed14320a0f5
raveclassic <raveclassic@gmail.com>
2022-12-29 11:40:19 +0300

    feat(analyzer): injectable-ts normalized graph node // server

--- packages/analyzer/src/server/server.ts
+++ packages/analyzer/src/server/server.ts
@@ -1,12 +1,12 @@
-// import fs from 'fs'
+import fs from 'fs'
 import path from 'path'
-// import express from 'express'
-// import { createServer as createViteServer } from 'vite'
+import express from 'express'
+import { createServer as createViteServer } from 'vite'
 import { getInjectableCore } from './injectable-ts-core'
 import { buildGraph } from './graph'
 import { newProject } from './project'
 
-// const SELF = path.resolve(__dirname)
+const SELF = path.resolve(__dirname)
 
 const PROJECT_ROOT =
   '/Users/raveclassic/WebstormProjects/dexscreener/ds/packages/util-network'
@@ -16,66 +16,81 @@ const PROJECT_TS_CONFIG = path.resolve(PROJECT_ROOT, 'tsconfig.lib.json')
 //   '../../node_modules/@injectable-ts/core/src/index.d.ts'
 // )
 //
-// async function createServer() {
-//   const app = express()
-//
-//   // Create Vite server in middleware mode and configure the app type as
-//   // 'custom', disabling Vite's own HTML serving logic so parent server
-//   // can take control
-//   const vite = await createViteServer({
-//     server: { middlewareMode: true },
-//     appType: 'custom',
-//   })
-//
-//   // use vite's connect instance as middleware
-//   // if you use your own express router (express.Router()), you should use router.use
-//   app.use(vite.middlewares)
-//
-//   app.use('*', async (req, res, next) => {
-//     const url = req.originalUrl
-//
-//     try {
-//       // 1. Read index.html
-//       let template = fs.readFileSync(
-//         path.resolve(SELF, '../../index.html'),
-//         'utf-8'
-//       )
-//
-//       // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
-//       //    also applies HTML transforms from Vite plugins, e.g. global preambles
-//       //    from @vitejs/plugin-react
-//       template = await vite.transformIndexHtml(url, template)
-//
-//       // 3. Load the server entry. vite.ssrLoadModule automatically transforms
-//       //    your ESM source code to be usable in Node.js! There is no bundling
-//       //    required, and provides efficient invalidation similar to HMR.
-//       const { render } = await vite.ssrLoadModule(
-//         path.resolve(SELF, 'entry.tsx')
-//       )
-//
-//       // 4. render the app HTML. This assumes entry-server.js's exported `render`
-//       //    function calls appropriate framework SSR APIs,
-//       //    e.g. ReactDOMServer.renderToString()
-//       const appHtml = await render(url)
-//
-//       // 5. Inject the app-rendered HTML into the template.
-//       const html = template.replace(`<!--ssr-outlet-->`, appHtml)
-//
-//       // 6. Send the rendered HTML back.
-//       res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
-//     } catch (e) {
-//       // If an error is caught, let Vite fix the stack trace so it maps back to
-//       // your actual source code.
-//       vite.ssrFixStacktrace(e instanceof Error ? e : new Error(`${e}`))
-//       next(e)
-//     }
-//   })
-//
-//   app.listen(5173)
-// }
-//
-// void createServer()
-const project = newProject(PROJECT_TS_CONFIG)
-const core = getInjectableCore(project)
-const graph = buildGraph(project, core)
-console.log(graph)
+async function main(): Promise<void> {
+  const app = express()
+
+  // Create Vite server in middleware mode and configure the app type as
+  // 'custom', disabling Vite's own HTML serving logic so parent server
+  // can take control
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+  })
+
+  // use vite's connect instance as middleware
+  // if you use your own express router (express.Router()), you should use router.use
+  app.use(vite.middlewares)
+
+  app.get('/graph', async (req, res, next) => {
+    try {
+      const project = newProject(PROJECT_TS_CONFIG)
+      const core = getInjectableCore(project)
+      const graph = buildGraph(project, core)
+
+      res
+        .status(200)
+        .set({ 'Content-Type': 'application/json' })
+        .end(JSON.stringify(graph))
+    } catch (e) {
+      res.status(500).end(`${e}`)
+      // next(e)
+    }
+  })
+
+  app.use('*', async (req, res, next) => {
+    const url = req.originalUrl
+
+    try {
+      // 1. Read index.html
+      let template = fs.readFileSync(
+        path.resolve(SELF, '../../index.html'),
+        'utf-8'
+      )
+
+      // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
+      //    also applies HTML transforms from Vite plugins, e.g. global preambles
+      //    from @vitejs/plugin-react
+      template = await vite.transformIndexHtml(url, template)
+
+      // 3. Load the server entry. vite.ssrLoadModule automatically transforms
+      //    your ESM source code to be usable in Node.js! There is no bundling
+      //    required, and provides efficient invalidation similar to HMR.
+      // const { render } = await vite.ssrLoadModule(
+      //   path.resolve(SELF, 'entry.tsx')
+      // )
+
+      // 4. render the app HTML. This assumes entry-server.js's exported `render`
+      //    function calls appropriate framework SSR APIs,
+      //    e.g. ReactDOMServer.renderToString()
+      // const appHtml = await render(url)
+
+      // 5. Inject the app-rendered HTML into the template.
+      // const html = template.replace(`<!--ssr-outlet-->`, appHtml)
+      const html = template.replace(`<!--ssr-outlet-->`, '')
+
+      // 6. Send the rendered HTML back.
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+    } catch (e) {
+      // If an error is caught, let Vite fix the stack trace, so it maps back to
+      // your actual source code.
+      vite.ssrFixStacktrace(e instanceof Error ? e : new Error(`${e}`))
+      next(e)
+    }
+  })
+
+  const server = app.listen(5173, () => {
+    console.log('Listening on', server.address())
+  })
+}
+
+void main()
