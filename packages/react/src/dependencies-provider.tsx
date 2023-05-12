c453f1370ad64edf6f7b51791c7c87c38f2b367a
raveclassic <raveclassic@gmail.com>
2023-05-12 16:55:34 +0300

    perf(react): remove unnecessary memo

--- packages/react/src/dependencies-provider.tsx
+++ packages/react/src/dependencies-provider.tsx
@@ -1,6 +1,6 @@
 import { TOKEN_ACCESSOR_KEY, TokenAccessor } from '@injectable-ts/core'
 import * as React from 'react'
-import { memo, ProviderProps, useContext, useMemo } from 'react'
+import { ProviderProps, useContext, useMemo } from 'react'
 import { context, UnknownDependencies } from './context'
 
 const checkedAccessor: TokenAccessor = (dependencies, name) => {
@@ -9,22 +9,22 @@ const checkedAccessor: TokenAccessor = (dependencies, name) => {
   }
   throw new Error(`Missing dependency: ${JSON.stringify(name)}`)
 }
-export const DependenciesProvider = memo(
-  (props: ProviderProps<UnknownDependencies>) => {
-    const previousDependencies = useContext(context)
-    const mergedDependencies = useMemo(
-      () => ({
-        [TOKEN_ACCESSOR_KEY]: checkedAccessor,
-        ...previousDependencies,
-        ...props.value,
-      }),
-      [previousDependencies, props.value]
-    )
-    return (
-      <context.Provider value={mergedDependencies}>
-        {props.children}
-      </context.Provider>
-    )
-  }
-)
+export const DependenciesProvider = (
+  props: ProviderProps<UnknownDependencies>
+) => {
+  const previousDependencies = useContext(context)
+  const mergedDependencies = useMemo(
+    () => ({
+      [TOKEN_ACCESSOR_KEY]: checkedAccessor,
+      ...previousDependencies,
+      ...props.value,
+    }),
+    [previousDependencies, props.value]
+  )
+  return (
+    <context.Provider value={mergedDependencies}>
+      {props.children}
+    </context.Provider>
+  )
+}
 DependenciesProvider.displayName = 'DependenciesProvider'
